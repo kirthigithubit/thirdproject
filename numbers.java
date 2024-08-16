d3ce498a1b0ae222b519c6e1676e8859443d10f1
kirthigithubit <kiruthikaraju7@gmail.com>
2024-08-16 12:50:41 +0530

    added file

---
+++ numbers.java
@@ -0,0 +1,38 @@
+public class numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Odd Numbers");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the n numbers:");	
+		int no = sc.nextInt();
+	    int[] a=new int[no];
+	    for(int i=0;i<no;i++)
+	    {
+	    	a[i]=sc.nextInt();
+	    }
+	    for(int i=0;i<no;i++)
+	    {
+	    	if(a[i]%3==0)
+	    	{
+	    	 System.out.println("Odd number is "+a[i]);
+	    	}
+	    }
+		System.out.println("Even Numbers");
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter the n numbers:");	
+		int n = in.nextInt();
+	    int[] array=new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	    	array[i]=in.nextInt();
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	    	if(array[i]%2==0)
+	    	{
+	    	 System.out.println("Even number is "+array[i]);
+	    	}
+	    }
+	}
+}
